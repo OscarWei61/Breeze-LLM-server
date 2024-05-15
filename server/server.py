bf1a3668b81841a654a42ee23e84096ed8b48ac2
OscarWei61 <wei573434@gmail.com>
2024-05-15 23:57:35 +0800

    fix: generate_schedule bugs

--- server/server.py
+++ server/server.py
@@ -27,13 +27,8 @@ def generate_advices(disease_name: str, disease_factor: str, patient_customized_
 
 # Endpoint to generate schedule based on disease name, factor, and customized content
 @router.get("/generate_schedule")
-<<<<<<< HEAD
 def generate_schedule(disease_name: str, disease_factor: str, patient_customized_content: str):
     prompt_input = "我的檢查結果有 " + str(disease_name) + " 問題，是因為有 " + str(disease_factor) + " 的因素，主要可能的原因以及改善的方式有 " + patient_customized_content + "可以幫我只依照以上的內容輸出建議我改善生活狀態的排成，我需要一周的計劃表(周一至周日)，我希望一天以早上、下午、晚上三個時段來區隔，主要以餐點以及運動來給予建議，並且以markdown的語法來幫我製作表格以及建議執行的內容應該要多元化，餐點(早餐、中餐、晚餐)要依照情況進行客製化，同一份規畫不能重複超過三次，來安排我未來一周的行程"
-=======
-def generate_text(disease_name: str, disease_factor: str, patient_customized_content: str):
-    prompt_input = "我的檢查結果有 " + str(disease_name) + " 問題，是因為有 " + str(disease_factor) + " 的因素，主要可能的原因以及改善的方式有 " + patient_customized_content + "可以幫我只依照以上的內容輸出建議我改善生活狀態的排成，我需要一周的計劃表(周一至周日)，我希望一天以早餐、中餐、晚餐來區隔，主要以餐點以及每日一次的運動來給予建議，並且以json的e格式來幫我製作表格以及建議執行的內容應該要多元化，餐點(早餐、中餐、晚餐)要依照情況進行客製化，同一份規畫不能重複超過三次，來安排我未來一周的行程"
->>>>>>> 8110292eb4d3683a9ece7929967a3694b7c0b13e
     output_string = Breeze(prompt_input)
     return JSONResponse(content={"result": output_string})
 
